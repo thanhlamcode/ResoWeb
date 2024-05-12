386dea8c89f3046f93a40c6c1856cf2437d89ac5
thanhlamcode <lamdoan1122334455@gmail.com>
2024-05-12 11:55:34 +0700

    update

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,13 +1,18 @@
 var create__right_item = document.querySelectorAll(".create__right--item");
-
+var create__bottom__item = document.querySelectorAll(".create__bottom--item");
 console.log(create__right_item);
 
-create__right_item.forEach(item => {
+create__right_item.forEach((item, index) => {
     item.addEventListener("click",()=>{
         create__right_item.forEach(otherItem => {
             otherItem.classList.remove("focus");
         });
 
         item.classList.add("focus");
+
+        create__bottom__item.forEach(bottomItem => {
+            bottomItem.classList.remove("display");
+        });
+        create__bottom__item[index].classList.add("display");
     })
 });
